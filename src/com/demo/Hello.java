8133a1ced43a4a6db06c5e7a47b37450004a7066
liqing6044313 <liqing_work@163.com>
2017-12-05 14:00:44 +0800

    change package

--- src/com/demo/Hello.java
+++ src/com/demo/Hello.java
@@ -3,6 +3,6 @@ package com.demo;
 public class Hello {
     public static void main(String args[])
     {
-        System.out.println(" first1 idea demo 2");
+        System.out.println(" first1 idea demo 2  11");
     }
 }
