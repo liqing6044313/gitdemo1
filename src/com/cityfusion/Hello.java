044a0feefb2511a4547a932b790ae021bbe1db63
liqing6044313 <liqing_work@163.com>
2017-12-05 10:53:57 +0800

    first commit 2

--- src/com/cityfusion/Hello.java
+++ src/com/cityfusion/Hello.java
@@ -5,6 +5,6 @@ import org.apache.commons.collections.bag.SynchronizedSortedBag;
 public class Hello {
     public static void main(String args[])
     {
-        System.out.println("first idea demo");
+        System.out.println("first idea demo 2");
     }
 }
